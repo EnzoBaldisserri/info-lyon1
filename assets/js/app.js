b6dc53b240a675a2a69a2ad03255104a4c20c027
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2018-05-13 16:16:37 +0200

    Add simple js confirmation dialog

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import '../scss/app.scss';
 
 window.addEventListener('load', () => {
+  // Navigation
   M.Dropdown.init(
     document.getElementById('navbar-user-trigger'),
     {
@@ -11,4 +12,14 @@ window.addEventListener('load', () => {
   );
 
   M.Sidenav.init(document.getElementById('mobile-sidenav'));
+
+  // Form confirmation
+  // eslint-disable-next-line no-alert
+  const confirm = e => window.confirm(e.target.getAttribute('data-confirm'));
+
+  document.querySelectorAll('a[data-confirm]')
+    .forEach($anchor => $anchor.addEventListener('click', confirm));
+
+  document.querySelectorAll('form[data-confirm]')
+    .forEach($form => $form.addEventListener('submit', confirm));
 });
