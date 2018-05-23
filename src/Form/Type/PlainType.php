5c3a83439989e56f973b4fd2ac495ce171e0cf6b
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2018-05-23 21:45:56 +0200

    Create PlainType for displaying text in forms

---
+++ src/Form/Type/PlainType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlainType extends AbstractType
+{
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $value = $form->getViewData();
+
+        $view->vars['value'] = (string) $value;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'mapped' => false,
+        ]);
+    }
+}
