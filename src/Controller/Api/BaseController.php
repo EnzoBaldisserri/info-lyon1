58d6b8474145b7afa0c7ed9af142ebca3f0eccba
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2018-06-02 16:44:09 +0200

    Fix (AGAIN) serialization for route api_absence_get_all

--- src/Controller/Api/BaseController.php
+++ src/Controller/Api/BaseController.php
@@ -4,24 +4,17 @@ namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use JMS\Serializer\SerializerInterface;
 
 abstract class BaseController extends AbstractController
 {
-    private $serializer;
-
-    public function __construct(SerializerInterface $serializer)
-    {
-        $this->serializer = $serializer;
-    }
-
     protected function createJsonResponse(Array $data = [], int $status = 200, Array $headers = [])
     {
         if (empty($data)) {
             $data['ok'] = $status === 200;
         }
 
-        $json = $this->serializer->serialize($data, 'json');
+        $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
+        $json = $serializer->serialize($data, 'json');
 
         return JsonResponse::fromJsonString($json, $status, $headers);
     }
