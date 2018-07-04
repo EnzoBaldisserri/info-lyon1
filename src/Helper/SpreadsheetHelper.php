a0a88341e7f0f19735c24b6628d8dd35fa08699a
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2018-07-04 22:26:32 +0200

    Download semester or sample file (untested)

--- src/Helper/SpreadsheetHelper.php
+++ src/Helper/SpreadsheetHelper.php
@@ -2,15 +2,35 @@
 
 namespace App\Helper;
 
+use App\Entity\Administration\Group;
 use App\Entity\Administration\Semester;
+use App\Entity\User\Student;
+use App\Repository\Administration\CourseRepository;
+use App\Repository\Administration\SemesterRepository;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use Symfony\Component\Translation\TranslatorInterface;
+use Doctrine\Common\Collections\Collection;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Exception as SpreadsheetException;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\Writer\Exception as WriteException;
 use PhpOffice\PhpSpreadsheet\Reader\Exception as ReadException;
 use SplFileInfo;
 
 class SpreadsheetHelper
 {
+    private $translator;
+    private $courseRepository;
+    private $semesterRepository;
+
+    public function __construct(TranslatorInterface $translator, CourseRepository $courseRepository, SemesterRepository $semesterRepository)
+    {
+        $this->translator = $translator;
+        $this->courseRepository = $courseRepository;
+        $this->semesterRepository = $semesterRepository;
+    }
+
     /**
      * Parse the file to a spreadsheet.
      * It can be an Xls, Xlsx, Xml, Ods, Slk, Gnumeric, Html or Csv file.
@@ -62,15 +82,76 @@ class SpreadsheetHelper
      *
      * @param Semester $semester The semester
      * @return Spreadsheet The spreadsheet
+     *
+     * @throws SpreadsheetException
      */
     public function createForSemester(Semester $semester): Spreadsheet
     {
+        $spreadsheet = new Spreadsheet();
 
+        $worksheet = $spreadsheet->getActiveSheet();
+
+        $this->writeHeader($worksheet);
+        $this->writeProperties($worksheet, $semester);
+        $this->writeFooter($worksheet);
+
+        // Groups
+        return $spreadsheet;
     }
 
+    /**
+     * Create a sample spreadsheet.
+     *
+     * @return Spreadsheet
+     *
+     * @throws SpreadsheetException
+     */
     public function createForSemesterSample(): Spreadsheet
     {
+        $nextPeriod = $this->semesterRepository->findNextPeriod();
 
+        $lastBeginningCourse = $this->courseRepository->findOneBy(
+            [ 'semester' => 1 ],
+            [ 'id' => 'DESC']
+        );
+
+        /** @var Student $sampleStudent1 */
+        $sampleStudent1 = (new Student())
+            ->setUsername('p0000001')
+            ->setFirstname('John')
+            ->setSurname('Doe');
+
+        /** @var Student $sampleStudent2 */
+        $sampleStudent2 = (new Student())
+            ->setUsername('p0000012')
+            ->setFirstname('Richard')
+            ->setSurname('Roe');
+
+        $sampleGroup1 = (new Group())
+            ->setNumber(1)
+            ->addStudent($sampleStudent1)
+            ->addStudent($sampleStudent2)
+        ;
+
+        /** @var Student $sampleStudent3 */
+        $sampleStudent3 = (new Student)
+            ->setUsername('p0000008')
+            ->setFirstname('Jane')
+            ->setSurname('Doe');
+
+        $sampleGroup2 = (new Group())
+            ->setNumber(2)
+            ->addStudent($sampleStudent3)
+        ;
+
+        $sampleSemester = (new Semester())
+            ->setPeriod($nextPeriod)
+            ->setCourse($lastBeginningCourse)
+            ->addGroup($sampleGroup1)
+            ->addGroup($sampleGroup2)
+        ;
+
+        return $this->createForSemester($sampleSemester);
     }
 
     /**
@@ -100,5 +181,161 @@ class SpreadsheetHelper
         $this->updateSemester($spreadsheet, $semester);
 
         return $semester;
+    }
+
+    /**
+     * @param Cell $cell
+     * @param string $style
+     *
+     * @throws SpreadsheetException
+     */
+    protected function setCellStyle(Cell $cell, string $style)
+    {
+        static $styles = [
+            'bad' => [
+                'fill' => [
+                    'color' => ['argb' => 'FF9C0006'],
+                ],
+                'font' => [
+                    'color' => ['argb' => 'FFFFC7CE'],
+                ],
+            ],
+            'good' => [
+                'fill' => [
+                    'color' => ['argb' => 'FFC6EFCE'],
+                ],
+                'font' => [
+                    'color' => ['argb' => 'FF006100'],
+                ],
+            ],
+        ];
+
+        $style = strtolower($style);
+
+        if (!isset($styles[$style])) {
+            throw new SpreadsheetException('Style does not exist');
+        }
+
+        $cell->getStyle()->applyFromArray($styles[$style]);
+    }
+
+    protected function writeHeader(Worksheet $worksheet): void
+    {
+        $worksheet->setCellValue(
+            'A1', $this->translator->trans('semester.file.title.property'));
+        $worksheet->setCellValue(
+            'B1', $this->translator->trans('semester.file.title.value'));
+        $worksheet->setCellValue(
+            'C1', $this->translator->trans('semester.file.title.more_infos'));
+    }
+
+    /**
+     * @param Worksheet $worksheet
+     * @param Semester $semester
+     *
+     * @throws SpreadsheetException
+     */
+    protected  function writeProperties(Worksheet $worksheet, Semester $semester): void
+    {
+        // id
+        $worksheet->setCellValue(
+            'A2', 'id');
+        $cell = $worksheet->getCell('B2');
+        $cell->setValue($semester->getId() ?? '');
+        $this->setCellStyle($cell, 'bad');
+
+        // Course type
+        $worksheet->setCellValue(
+            'A3', $this->translator->trans('course.props.semester'));
+        $worksheet->setCellValue(
+            'B3', $semester->getCourse()->getSemester());
+
+        // Course PPN
+        $worksheet->setCellValue(
+            'A4', $this->translator->trans('course.props.implementationYear'));
+        $worksheet->setCellValue(
+            'B4', $semester->getCourse()->getImplementationDate()->format('Y'));
+
+        // Start date
+        $worksheet->setCellValue(
+            'A5', $this->translator->trans('semester.props.start_date'));
+        $worksheet->setCellValue(
+            'B5', $semester->getStartDate()->format('Y-m-d'));
+
+        // End date
+        $worksheet->setCellValue(
+            'A6', $this->translator->trans('semester.props.end_date'));
+        $worksheet->setCellValue(
+            'B6', $semester->getEndDate()->format('Y-m-d'));
+
+        // Groups
+        $this->writeGroups($worksheet, $semester->getGroups());
+    }
+
+    /**
+     * @param Worksheet $worksheet
+     * @param Group[]|Collection $groups
+     *
+     * @throws SpreadsheetException
+     */
+    private function writeGroups(Worksheet $worksheet, Collection $groups): void
+    {
+        $row = $worksheet->getHighestRow() + 2;
+
+        foreach ($groups as $group) {
+            // Group name
+            $worksheet->setCellValue(
+                'A'.$row,
+                $this->translator->trans('group.entity', [
+                    '%number%' => $group->getNumber(),
+                ])
+            );
+
+            $row += 1;
+
+            // id
+            $worksheet->setCellValue(
+                'A'.$row, 'id');
+            $cell = $worksheet->getCell('B'.$row);
+            $cell->setValue($group->getId() ?? '');
+            $this->setCellStyle($cell, 'bad');
+
+            $row += 1;
+
+            // Students
+            $worksheet->setCellValue(
+                'A'.$row, $this->translator->trans('group.props.students'));
+
+            foreach ($group->getStudents() as $student) {
+                $worksheet->setCellValue(
+                    'B'.$row, $student->getUsername());
+                $worksheet->setCellValue(
+                    'C'.$row, $student->getFirstname());
+                $worksheet->setCellValue(
+                    'D'.$row, $student->getSurname());
+
+                $row += 1;
+            }
+
+            $row += 2;
+        }
+    }
+
+    /**
+     * @param Worksheet $worksheet
+     * @throws SpreadsheetException
+     */
+    protected  function writeFooter(Worksheet $worksheet): void
+    {
+        $row = $worksheet->getHighestRow() + 2;
+
+        $cell = $worksheet->getCell('A'.$row);
+        $cell->setValue($this->translator->trans('semester.file.footer.no_modify'));
+        $this->setCellStyle($cell, 'bad');
+
+        $row += 1;
+
+        $worksheet->setCellValue(
+            'A'.$row, $this->translator->trans('semester.file.footer.analyse_value'));
     }
 }
